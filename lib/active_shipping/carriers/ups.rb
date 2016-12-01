22e14f1ed670d58e5301f9953937ddb12bce8c27
Adnan Abdulally <adnan@mgxcopy.com>
2016-12-01 15:03:58 -0800

    Update ups.rb
    update shipping method names to be consistent with UPS

--- lib/active_shipping/carriers/ups.rb
+++ lib/active_shipping/carriers/ups.rb
@@ -51,14 +51,14 @@ module ActiveShipping
 
     DEFAULT_SERVICES = {
       "01" => "UPS Next Day Air",
-      "02" => "UPS Second Day Air",
+      "02" => "UPS 2nd Day Air",
       "03" => "UPS Ground",
       "07" => "UPS Worldwide Express",
       "08" => "UPS Worldwide Expedited",
       "11" => "UPS Standard",
-      "12" => "UPS Three-Day Select",
+      "12" => "UPS 3 Day Select",
       "13" => "UPS Next Day Air Saver",
-      "14" => "UPS Next Day Air Early A.M.",
+      "14" => "UPS Next Day Air Early",
       "54" => "UPS Worldwide Express Plus",
       "59" => "UPS Second Day Air A.M.",
       "65" => "UPS Saver",
